02d93431e476439e6b91c5420e7667ff2c378749
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-11-21 20:25:18 +0530

    commit add user and sign

--- src/Services/apiClient.ts
+++ src/Services/apiClient.ts
@@ -2,7 +2,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api"
+  baseURL: "https://zanaapi-1.onrender.com/api"
+  ,
+
   headers: { "Content-Type": "application/json" },
 });
 
