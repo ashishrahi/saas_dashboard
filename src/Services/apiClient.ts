1af4c09ab5b99823ed94d32f59847333a404dfe5
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2026-01-04 21:54:50 +0530

    add urlrender

--- src/Services/apiClient.ts
+++ src/Services/apiClient.ts
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api"
-  // baseURL: "https://zanaapi-1.onrender.com/api"
+  // baseURL: "http://localhost:5000/api"
+  baseURL: "https://zanaapi-1.onrender.com/api"
   ,
 
   headers: { "Content-Type": "application/json" },
